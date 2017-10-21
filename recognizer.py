f9b393911d9ab3b78f956545f861abd5f253f70f
Abelarm <luigi3000@gmail.com>
2017-10-21 19:04:09 +0200

    [fix] created function for making the code more readable and added some fix improving the quality of output images

--- recognizer.py
+++ recognizer.py
@@ -3,28 +3,18 @@ import cv2
 import numpy as np
 import os.path
 import itertools
-import pytesseract
-from PIL import Image
+#import pytesseract
+#from PIL import Image
 
-filename = "./dataset/Test2.png"
+filename = "./dataset/squaresub.jpg"
 
-if os.path.isfile(filename):
 
-    image = cv2.imread(filename, flags=cv2.IMREAD_COLOR)
-    cv2.imshow(filename.split('/')[-1], image)
-    startimage = image.copy()
-
-    low_bound = (0, int(image.shape[0]*0.80))
-    upper_bound = (image.shape[0], image.shape[1])
-    print low_bound
-
-    cv2.circle(image, low_bound, 5, (0, 255, 0), 2)
-    # cv2.imshow('low_bound', image)
+def get_contours(image, bound, comparator):
 
     blur = cv2.GaussianBlur(image, (9, 9), 0)
     edges = cv2.Canny(blur, 100, 200)
 
-    # cv2.imshow('Canny_Blur', edges)
+    cv2.imshow('Canny_Blur', edges)
 
     contours = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
@@ -34,19 +24,19 @@ if os.path.isfile(filename):
     for i in range(len(contours)):
         flag = False
         for p in contours[i]:
-            if p[0, 1] < low_bound[1]:
+            if comparator(p[0, 1], bound[1]):
                 flag = True
                 break
         if not flag:
             good_contours.append(contours[i])
 
-    # np.asarray(good_contours)
+    if len(good_contours) == 0:
+        return good_contours
+
     cv2.drawContours(image, good_contours, -1, (0, 255, 255))
-    #cv2.imshow('GoodContours', image)
+    #cv2.imshow(image, "what2")
 
     merged = []
-
-
     centroids = []
     for cnt in good_contours:
         M = cv2.moments(cnt)
@@ -68,28 +58,26 @@ if os.path.isfile(filename):
 
     print("Mean distance: {}".format(mean_distance))
 
-    #print(distances)
-
-
     for i, g_c in enumerate(good_contours):
         test_image = image.copy()
         x, y, w, h = cv2.boundingRect(g_c)
 
         cv2.rectangle(test_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        #cv2.imshow("Test image", test_image)
+        mesure = 0
 
         if w > h:
             mesure = (float(w-h))/w
             if mesure > 0.8:
-                # print("Rectangle x:{} y:{} || width:{} height:{} diff:{}".format(x, y, w, h, mesure))
                 print("########FUNDED TOO BIG########")
                 good_contours.pop(i)
+
+
         if h > w:
             mesure = (float(h-w))/h
             if mesure > 0.8:
-                # print("Rectangle x:{} y:{} || width:{} height:{} diff:{}".format(x, y, w, h, mesure))
                 print("########FUNDED TOO BIG########")
                 good_contours.pop(i)
+
 
         M = cv2.moments(g_c)
         if M['m00'] != 0:
@@ -103,26 +91,23 @@ if os.path.isfile(filename):
                 if list(centroid) != list(c):
                     dist += np.linalg.norm(centroid - np.array(c))
                     i += 1
-
-            dist /= i
-            mesure = (float(dist-mean_distance))/dist
-
-        #print("Rectangle x:{} y:{} centroid {}|| width:{} height:{} dist:{} meas:{}".format(x, y, centroid, w, h, dist, mesure))
+            if i > 0:
+                dist /= i
+                mesure = (float(dist-mean_distance))/dist
 
         if mesure > 0.5:
             print("########FUNDED TOO DISTANT########")
             good_contours.pop(i)
 
-        # cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        #cv2.imshow('Debug contour', image)
-        # cv2.waitKey(0)
+        for x in g_c:
+            merged.append(x)
 
-        for bho in g_c:
-            merged.append(bho)
+    return merged
 
-    # merged = [gg_cc for g_c in good_contours for gg_cc in g_c]
 
-    merged = np.array(merged)
+def retrive_smallest_rect(image, contours):
+
+    merged = np.array(contours)
     hull = cv2.convexHull(merged)
 
     x, y, w, h = cv2.boundingRect(hull)
@@ -132,45 +117,78 @@ if os.path.isfile(filename):
     bigy = y + h
     cutted_image = startimage[y:bigy, x:bigx]
 
-    #cv2.imshow('Hull contours', image)
-    #cv2.imshow('Partial result', cutted_image)
+    cv2.imshow('Hull contours', image)
+    cv2.imshow('Partial result', cutted_image)
 
-    canny = cv2.Canny(cutted_image, 400, 800)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (6, 6))
+    canny = cv2.Canny(cutted_image, 200, 500)
+    cv2.imshow('SUBCANNY', canny)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     dilated = cv2.dilate(canny, kernel)
     contours = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
     mask = np.zeros((h, w), dtype=np.uint8)
     cv2.drawContours(mask, contours, -1, 255, thickness=-1)
-    # cv2.imshow("mask", mask)
-
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.erode(mask, kernel, iterations=1)
+    cv2.imshow("mask", mask)
 
     merged = [gg_cc for g_c in contours for gg_cc in g_c]
     merged = np.array(merged)
     hull = cv2.convexHull(merged)
 
     x, y, w, h = cv2.boundingRect(hull)
-    bigx = x + w
-    bigy = y + h
-    # mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    upper_x = x + w
+    upper_y = y + h
     print(mask.shape)
     print(cutted_image.shape)
 
-    aa = cv2.bitwise_and(cutted_image, cutted_image, mask=mask)
-    final_final = aa[y:bigy, x:bigx]
+    maksed = cv2.bitwise_and(cutted_image, cutted_image, mask=mask)
+    final_image = maksed[y:upper_y, x:upper_x]
+    cv2.imshow("masked", final_image)
     # print(final_final.shape)
-    final = cv2.cvtColor(final_final, cv2.COLOR_BGR2GRAY)
-    _, final = cv2.threshold(final, 150, 255, cv2.THRESH_BINARY)
-    # final = cv2.erode(final, (3, 3), iterations=1)
-    # final = cv2.dilate(final, (3, 3), iterations=1)
-    #print(final)
-    # cv2.imshow("final_no_thres", final)
-    cv2.imwrite('Test1.png', final)
+    final_tresh = cv2.cvtColor(final_image, cv2.COLOR_BGR2GRAY)
+    _, final_tresh = cv2.threshold(final_tresh, 135, 255, cv2.THRESH_BINARY)
+    #final_tresh = cv2.GaussianBlur(final_tresh, (3, 3), 1, 1)
 
-    img = Image.fromarray(final)
-    print(pytesseract.image_to_string(img, 'eng'))
+    return final_tresh
+
+
+
+if os.path.isfile(filename):
+
+    image = cv2.imread(filename, flags=cv2.IMREAD_COLOR)
+    cv2.imshow(filename.split('/')[-1], image)
+    startimage = image.copy()
+
+    lower_bound = (0, int(image.shape[0]*0.80))
+    upper_bound = (0, int(image.shape[0]*0.20))
+
+    cv2.circle(image, lower_bound, 5, (0, 255, 0), 2)
+    #cv2.imshow('low_bound', image)
+    cv2.circle(image, upper_bound, 5, (0, 255, 0), 2)
+    cv2.imshow('bounds', image)
+
+    contours_up = get_contours(image, upper_bound, lambda x, y: x > y)
+    contours_down = get_contours(image, lower_bound, lambda x, y: x < y)
+
+    if len(contours_up) == 0 and len(contours_down) > 0:
+        contours = contours_down
+    elif len(contours_up) > 0 and len(contours_down) == 0:
+        contours = contours_up
+    else:
+        contours = contours_down
+
+    final_tresh = retrive_smallest_rect(image, contours)
+
+    cv2.imshow("final_final", final_tresh)
+    cv2.imwrite('Test1.png', final_tresh)
+
+    #img = Image.fromarray(final)
+    #print(pytesseract.image_to_string(img, 'eng'))
     cv2.waitKey(0)
     exit()
+
+
 
 
     '''
